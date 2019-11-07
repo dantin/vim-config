f6617d5bfe45f5711c746c089d29072c903bfb54
dantin <chengjie.ding@gmail.com>
2019-11-07 10:14:11 +0800

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+# sudo yum install -y python-devel python3-devel
 
-############################  SETUP PARAMETERS
-ROOT_PATH=$(dirname "${BASH_SOURCE[0]}")
+cd $(dirname "${BASH_SOURCE[0]}")
+BASE_PATH="$(pwd)"
 
-############################  BASIC SETUP TOOLS
 msg() {
     printf '%b\n' "$1" >&2
 }
@@ -19,70 +19,37 @@ error() {
     exit 1
 }
 
-program_exists() {
-    local ret='0'
-    command -v $1 >/dev/null 2>&1 || { local ret='1';  }
-
-    # fail on non-zero return value
-    if [ "$ret" -ne 0  ]; then
-        return 1
-    fi
-
-    return 0
-}
-
-program_must_exist() {
-    program_exists $1
-
-    # throw error on non-zero return value
-    if [ "$?" -ne 0  ]; then
-        error "You must have '$1' installed to continue."
-    fi
-}
-
-argument_set() {
-    if [ -z "$1" ]; then
-        error "You must have your arguments set before continue."
-    fi
-}
-
-############################ SETUP FUNCTIONS
 download_resource() {
-    argument_set "$1"
-    argument_set "$2"
-    argument_set "$3"
+    local local_path="$1"
+    local remote_url="$2"
 
-    local resource_id="$1"
-    local local_path="$2"
-    local remote_url="$3"
-
-    curl -fLo $ROOT_PATH/$local_path --create-dirs $remote_url \
-        1>/dev/null 2>&1
+    curl -fLo $local_path --create-dirs $remote_url 1>/dev/null 2>&1
 
     ret="$?"
-    success "Plugin $resource_id downloaded."
+    success "Download complete"
 }
 
-update_plugin() {
-    local plugins_table=( \
-        "vim-plug, autoload/plug.vim, https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
-        "vim-monokai, colors/monokai.vim, https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim")
+#### main
 
 
-    for i in "${!plugins_table[@]}"; do
-        IFS=', ' read -r -a entry <<< "${plugins_table[i]}"
-        download_resource "${entry[0]}" \
-                          "${entry[1]}" \
-                          "${entry[2]}"
-    done
+# vim-monokai
+msg "Update 'vim-monokai'"
+download_resource "$BASE_PATH/colors/monokai.vim" "https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim"
 
-    ret="$?"
-    success "Successfully update plugins."
-}
+# vim-plug
+msg "Upgrade 'vim-plug'"
+vim +PlugUpgrade +qall 1>/dev/null 2>&1
 
-############################ MAIN()
-program_must_exist "curl"
+msg "Update Vim plugins"
+vim +PlugUpdate +qall 1>/dev/null 2>&1
 
-update_plugin
+msg "Rebuild Plugin LeaderF"
+cd $BASE_PATH/plugged/LeaderF
+./install.sh
 
-msg "Done!"
+msg "Rebuild Plugina YCM "
+cd $BASE_PATH/plugged/YouCompleteMe
+./install.sh
+
+ret="$?"
+success "Vim configuration updated"
